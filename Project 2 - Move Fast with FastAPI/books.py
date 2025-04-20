db05b59f286587b83cd4ee383a015d69f44533fc
Evan Hanif Widiatama <evan.hanif.w@gmail.com>
2025-04-20 18:46:53 +0700

    Add books.py file with FastAPI implementation for book management, including models and endpoints for retrieving and creating books.

---
+++ Project 2 - Move Fast with FastAPI/books.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+    
+    def __init__(self, id, title, author, description, rating, published_date) -> None:
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+
+
+app = FastAPI()
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2030),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 2029),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2028),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2027),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
