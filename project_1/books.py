24cc5992534e139025f648e6ae491da654615186
Evan Hanif Widiatama <evan.hanif.w@gmail.com>
2025-04-20 13:03:28 +0700

    Add new API endpoints to filter books by category and author with category

--- project_1/books.py
+++ project_1/books.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-
-BOOKS = [
+from typing import List, Dict
+    
+BOOKS:  List[Dict[str, str]] = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
     {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
@@ -17,7 +18,28 @@ async def read_all_books():
 
 @app.get('/books/{book_title}')
 async def read_book(book_title: str):
+    
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+        
+@app.get('/books/')
+async def read_category_by_query(category: str):
+    books_to_return = []
+    
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
         
