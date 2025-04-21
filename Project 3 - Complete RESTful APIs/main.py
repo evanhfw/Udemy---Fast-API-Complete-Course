2bb743434caabfcddcceff885bcf6408faf4174b
Evan Hanif Widiatama <evan.hanif.w@gmail.com>
2025-04-21 10:15:47 +0700

    Add CRUD endpoints for Todos in main.py
    Implement GET endpoints to retrieve all todos and a specific todo by ID. Introduce database session management with dependency injection using FastAPI's Depends. Enhance error handling for non-existent todo IDs.

--- Project 3 - Complete RESTful APIs/main.py
+++ Project 3 - Complete RESTful APIs/main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
+from starlette import status
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail=f"Todo ID {todo_id} not found.")
