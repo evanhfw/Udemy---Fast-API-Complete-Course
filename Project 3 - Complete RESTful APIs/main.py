0a181676c19ba522ca0289f199d33df955b2e32c
Evan Hanif Widiatama <evan.hanif.w@gmail.com>
2025-04-21 17:18:29 +0700

    Add CRUD functionality for Todos in main.py
    Introduce a new TodoRequest model with validation using Pydantic. Implement POST and PUT endpoints for creating and updating todos, enhancing the API's capabilities. Improve error handling for non-existent todo IDs and ensure consistent database session management.

--- Project 3 - Complete RESTful APIs/main.py
+++ Project 3 - Complete RESTful APIs/main.py
@@ -1,12 +1,13 @@
-from typing import Annotated
+"""FastAPI Todo application with SQLAlchemy ORM."""
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException
+from starlette import status
+
 import models
 from models import Todos
 from database import engine, SessionLocal
-from starlette import status
 
 app = FastAPI()
 
@@ -21,6 +22,13 @@ def get_db():
         db.close()
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=10, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: Session = Depends(get_db)):
     return db.query(Todos).all()
@@ -32,3 +40,28 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail=f"Todo ID {todo_id} not found.")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(todo_request: TodoRequest, db: Session = Depends(get_db)):
+    todo_model = Todos(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(
+    todo_id: int, todo_request: TodoRequest, db: Session = Depends(get_db)
+):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail=f"Todo ID {todo_id} not found.")
+
+    # type: ignore
+    todo_model.title = todo_request.title  # type: ignore
+    todo_model.description = todo_request.description  # type: ignore
+    todo_model.priority = todo_request.priority  # type: ignore
+    todo_model.complete = todo_request.complete  # type: ignore
+    db.add(todo_model)
+    db.commit()
+    return
