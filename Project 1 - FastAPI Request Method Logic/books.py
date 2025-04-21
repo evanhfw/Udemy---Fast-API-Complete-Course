dbd3385a82944f226de1273091fd1e0f77280938
Evan Hanif Widiatama <evan.hanif.w@gmail.com>
2025-04-21 10:15:14 +0700

    Refactor books.py to improve code readability by removing unnecessary whitespace and ensuring consistent formatting across API endpoints.

--- Project 1 - FastAPI Request Method Logic/books.py
+++ Project 1 - FastAPI Request Method Logic/books.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from typing import List, Dict
-    
+
 BOOKS:  List[Dict[str, str]] = [
     {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
     {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
@@ -12,14 +12,16 @@ BOOKS:  List[Dict[str, str]] = [
 
 app = FastAPI()
 
+
 @app.get("/books/fetch/")
 async def fetch_all_books_by_author_query(book_author: str):
     books_to_return = []
     for book in BOOKS:
         if book.get('author').casefold() == book_author.casefold():
             books_to_return.append(book)
-            
+
     return books_to_return
+
 
 @app.get("/books/fetch/{book_author}")
 async def fetch_all_books_by_author_path(book_author: str):
@@ -27,28 +29,31 @@ async def fetch_all_books_by_author_path(book_author: str):
     for book in BOOKS:
         if book.get('author').casefold() == book_author.casefold():
             books_to_return.append(book)
-            
+
     return books_to_return
+
 
 @app.get('/books')
 async def read_all_books():
     return BOOKS
 
+
 @app.get('/books/{book_title}')
 async def read_book(book_title: str):
-    
+
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
-        
+
+
 @app.get('/books/')
 async def read_category_by_query(category: str):
     books_to_return = []
-    
+
     for book in BOOKS:
         if book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
-    
+
     return books_to_return
 
 
@@ -60,17 +65,20 @@ async def read_author_category_by_query(book_author: str, category: str):
                 book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
-        
+
+
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
-    
+
+
 @app.put("/books/update_book")
 async def update_book(updated_book=Body()):
     for i in range(len(BOOKS)):
         if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
             BOOKS[i] = updated_book
-            
+
+
 @app.delete("/books/delete_book/{book_title}")
 async def delete_book(book_title: str):
     for i in range(len(BOOKS)):
